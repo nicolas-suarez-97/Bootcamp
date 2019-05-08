945f1bbba80f3b40a453b044ea450574b891fd55
Nicolas Suarez <nicolas.suarez10@hotmail.com>
2019-05-07 23:16:25 -0500

    SlidePuzzle

--- bogota-abril-2019/javascript-sliding-puzzle/Nicolas Suarez-Slide Puzzle/slide.js
+++ bogota-abril-2019/javascript-sliding-puzzle/Nicolas Suarez-Slide Puzzle/slide.js
@@ -1,56 +1,88 @@
-var tam = 4;
-var matrix = [];
 
-var con = 1;
-for(var i=0; i<tam; i++) {
-    matrix[i] = new Array(tam);    
-    for (var j=0; j<tam; j++){
-        matrix[i][j]=con;
-        if(con != tam*tam){
-            document.getElementById("button"+con).innerHTML=con;        
-            if(con=="2"||con=="4"||con=="5"||con=="7"||con=="10"||con=="12"||con=="13"||con=="15"){
-                document.getElementById("button"+con).style.backgroundColor="red";
-                document.getElementById("button"+con).style.color="white";
+var size = 16;
+var puzzleNumbers = [];
+var correctAnswer = [];
+
+for(var i=0; i<size; i++){
+    puzzleNumbers[i] = i+1; 
+    correctAnswer[i] = i+1; 
+}
+initializeButtons(puzzleNumbers);
+
+
+function randomize(){
+    puzzleNumbers.sort(()=>Math.random()-0.5);    
+    fillButtons(puzzleNumbers);
+    document.getElementById("win").style.display="none";    
+}
+
+function initializeButtons(array){
+    for(var i=0; i<size;i++){ 
+        var aux = i+1;       
+        if(array[i]!=size){
+        var aux = i+1;       
+            document.getElementById("button"+aux).innerHTML=array[i];        
+            if(aux=="2"||aux=="4"||aux=="5"||aux=="7"||aux=="10"||aux=="12"||aux=="13"||aux=="15"){
+                document.getElementById("button"+aux).style.backgroundColor="red";
+                document.getElementById("button"+aux).style.color="white";
+            }else{
+                document.getElementById("button"+aux).style.backgroundColor="white";
+                document.getElementById("button"+aux).style.color="black";
             }
         }else{
-            document.getElementById("button"+con).style.backgroundColor="white";
+            document.getElementById("button"+aux).style.backgroundColor="gray";
         }
-        con+=1;
     }
 }
 
-function move(val){
-    console.log(val);
-}
-
-
-function rand(){
-    console.log("rand");
-    for(var i=0;i<tam;i++){
-        matrix[i].sort(() => Math.random() - 0.5);
-    }
-    matrix.sort(() => Math.random() - 0.5);
-    fill(matrix);
-    console.log(matrix);
-}
-
-function fill(array){
-    var con = 1;
-    for(var i=0; i<tam; i++) {         
-        for (var j=0; j<tam; j++){            
-            if(con != tam*tam){
-                document.getElementById("button"+con).innerHTML=array[i][j];        
-                if(array[i][j]=="2"||array[i][j]=="4"||array[i][j]=="5"||array[i][j]=="7"||array[i][j]=="10"||array[i][j]=="12"||array[i][j]=="13"||array[i][j]=="15"){
-                    document.getElementById("button"+con).style.backgroundColor="red";
-                    document.getElementById("button"+con).style.color="white";
-                }else{
-                    document.getElementById("button"+con).style.backgroundColor="white";
-                    document.getElementById("button"+con).style.color="black";
-                }
+function fillButtons(array){       
+    for(var i=0; i<size;i++){
+        var aux = i+1;
+        if(array[i]!=size){
+            document.getElementById("button"+aux).innerHTML=array[i];        
+            if(array[i]=="2"||array[i]=="4"||array[i]=="5"||array[i]=="7"||
+            array[i]=="10"||array[i]=="12"||array[i]=="13"||array[i]=="15"){
+                document.getElementById("button"+aux).style.backgroundColor="red";
+                document.getElementById("button"+aux).style.color="white";
             }else{
-                document.getElementById("button"+con).style.backgroundColor="white";
+                document.getElementById("button"+aux).style.backgroundColor="white";
+                document.getElementById("button"+aux).style.color="black";
             }
-            con+=1;
+        }else{
+            document.getElementById("button"+aux).innerHTML=""; 
+            document.getElementById("button"+aux).style.backgroundColor="gray";
         }
+    }
+}
+
+
+function move(number){    
+    document.getElementById("win").style.display="none";
+    let index = puzzleNumbers.indexOf(16);        
+    if(number+1==index){
+        swap(index,number);
+    }
+    if(number-1==index){
+        swap(index,number);
+    }
+    if(number-4==index){
+        swap(index,number);
+    }
+    if(number+4==index){
+        swap(index,number);
+    }            
+    fillButtons(puzzleNumbers);
+    verify();
+}
+
+function swap(index,number){
+    let aux = puzzleNumbers[index];
+    puzzleNumbers[index]=puzzleNumbers[number];
+    puzzleNumbers[number]=aux;
+}
+
+function verify(){
+    if(correctAnswer.toString()== puzzleNumbers.toString()){
+        document.getElementById("win").style.display="flex";
     }
 }
